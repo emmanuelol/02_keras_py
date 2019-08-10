8aa71c768b72fc20a07f087be878788c48b088c1
riron1206 <rironyokoi@gmail.com>
2019-08-11 05:48:28 +0900

    add run_iPhone_img_predict.bat

---
+++ predicter/run_iPhone_img_predict.bat
@@ -0,0 +1,53 @@
+@rem 作成日2019/8/11 iPhone画像予測分類して予測クラスディレクトリにコピーする
+
+call activate tfgpu_py36_v3
+cd C:\Users\shingo\jupyter_notebook\tfgpu_py36_work\02_keras_py\predicter
+
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2016-01 -i D:\iPhone_pictures\2016-01
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2016-02 -i D:\iPhone_pictures\2016-02
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2016-03 -i D:\iPhone_pictures\2016-03
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2016-04 -i D:\iPhone_pictures\2016-04
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2016-05 -i D:\iPhone_pictures\2016-05
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2016-06 -i D:\iPhone_pictures\2016-06
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2016-07 -i D:\iPhone_pictures\2016-07
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2016-08 -i D:\iPhone_pictures\2016-08
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2016-09 -i D:\iPhone_pictures\2016-09
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2016-10 -i D:\iPhone_pictures\2016-10
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2016-11 -i D:\iPhone_pictures\2016-11
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2016-12 -i D:\iPhone_pictures\2016-12
+rem 
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2017-01 -i D:\iPhone_pictures\2017-01
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2017-02 -i D:\iPhone_pictures\2017-02
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2017-03 -i D:\iPhone_pictures\2017-03
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2017-04 -i D:\iPhone_pictures\2017-04
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2017-05 -i D:\iPhone_pictures\2017-05
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2017-06 -i D:\iPhone_pictures\2017-06
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2017-07 -i D:\iPhone_pictures\2017-07
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2017-08 -i D:\iPhone_pictures\2017-08
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2017-09 -i D:\iPhone_pictures\2017-09
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2017-10 -i D:\iPhone_pictures\2017-10
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2017-11 -i D:\iPhone_pictures\2017-11
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2017-12 -i D:\iPhone_pictures\2017-12
+rem 
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2018-01 -i D:\iPhone_pictures\2018-01
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2018-02 -i D:\iPhone_pictures\2018-02
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2018-03 -i D:\iPhone_pictures\2018-03
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2018-04 -i D:\iPhone_pictures\2018-04
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2018-05 -i D:\iPhone_pictures\2018-05
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2018-06 -i D:\iPhone_pictures\2018-06
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2018-07 -i D:\iPhone_pictures\2018-07
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2018-10 -i D:\iPhone_pictures\2018-10
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2018-11 -i D:\iPhone_pictures\2018-11
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2018-12 -i D:\iPhone_pictures\2018-12
+
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2019-01 -i D:\iPhone_pictures\2019-01
+rem call python output_pred_dir_cp.py -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\2019-02 -i D:\iPhone_pictures\2019-02
+
+@rem --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+@rem 作成日2019/8/11 iPhoneモデルで似ている画像を集めてタイル化（Reframe Visualization）
+
+call python reframe_visualization.py -i D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\merge\beagle -o D:\work\keras_iPhone_pictures\01_classes_results_tfgpu_py36\20190531\train_all\prediction_cp_img\merge\beagle
+
+
+
+rem pause
